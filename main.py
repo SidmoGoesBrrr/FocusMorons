40d941247d0b5af2788eb916504b8b13d85a1551
Frost <60550481+TechnoFrost27@users.noreply.github.com>
2022-01-28 09:20:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template
+from flask_sock import Sock
+app = Flask(__name__)
+sock=Sock(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    text = ""
+    if request.method == 'POST':
+        if request.form.get('bch') == 'bch':
+            pass
+
+
+    return render_template("index.html", text=text)
+
+
+@sock.route('/yo')
+def reverse(ws):
+  while True:
+    text=ws.receive()
+    ws.send(text[::-1])
+if __name__ == '__main__':
+    app.run('0.0.0.0')
